300d0eb00b42f8a8294909dabbdfa93d8ef80122
donhung <dotuyetnhung24@gmail.com>
2024-04-06 16:53:04 +0700

    search

--- model/san_pham.php
+++ model/san_pham.php
@@ -6,3 +6,16 @@ function all_list_sanpham()
     $sql = "SELECT sp_laptop.*, ten_loai, nhu_cau, mau_sac, kich_thuoc, ram FROM sp_laptop JOIN loai_laptop ON sp_laptop.ma_loai = loai_laptop.ma_loai ORDER BY ma_sp DESC";
     return  pdo_query($sql);
 }
+
+function loadall_sanpham($kyw = "", $ma_loai = 0)
+{
+    $sql = "SELECT * FROM sp_laptop WHERE 1";
+    if ($kyw != "") {
+        $sql .= " AND ten_sp like '%" . $kyw . "%'";
+    }
+    if ($ma_loai > 0) {
+        $sql .= " AND ma_loai = $ma_loai";
+    }
+    $sql .= " ORDER BY ma_sp DESC";
+    return  pdo_query($sql);
+}
